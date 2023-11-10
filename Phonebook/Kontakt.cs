4a34fe2b8d046e7d4d7c6f9d8b0ab49acff36f3d
daxi0 <daxi0@RADEK>
2023-11-10 15:43:19 +0100

    Add project files.

---
+++ Phonebook/Kontakt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    public class Kontakt
+    {
+        public string jmeno {  get; set; }
+        public string email { get; set;}
+        public string prace { get; set;}
+        public string telefon { get; set;}
+
+    }
+}
